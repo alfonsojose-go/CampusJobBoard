ad5384e4b2e993f264ea034718bff6b1aa5e5b19
alfonsojose-go <alfonsojose.go@rdpolytech.ca>
2025-12-04 13:39:51 -0700

    Initial project setup

---
+++ CampusJobBoard/src/test/java/com/example/CampusJobBoard/CampusJobBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CampusJobBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusJobBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
